46c420b1ba98bd6bb4965326cce3bdfbd6d7023a
IlyaKirillov <kocmohabt.baduk@gmail.com>
2016-06-05 03:39:10 +0300

    Улучшено отображение информации об игроке.

--- Src/KGS/Windows/UserInfo.js
+++ Src/KGS/Windows/UserInfo.js
@@ -34,14 +34,36 @@ CKGSUserInfoWindow.prototype.Init = function(sDivId, oPr)
 
 	var oMainDiv     = this.HtmlElement.InnerDiv;
 	var oMainControl = this.HtmlElement.InnerControl;
-
 	oMainDiv.style.fontSize = "16px";
 
-	this.m_oContainerDiv = this.protected_CreateDivElement(oMainDiv, sDivId + "C");
-	var oContainerControl = CreateControlContainer(sDivId + "C");
-	oContainerControl.Bounds.SetParams(5, 5, 5, 5, true, true, true, true, -1, -1);
-	oContainerControl.Anchor = (g_anchor_top |g_anchor_bottom | g_anchor_right | g_anchor_left);
-	oMainControl.AddControl(oContainerControl);
+	var sMainInfo  = sDivId + "M";
+	var sAvatar    = sDivId + "A";
+	var sExtension = sDivId + "E";
+
+	this.m_oMainInfoDiv = this.protected_CreateDivElement(oMainDiv, sMainInfo);
+	var oMainInfoControl = CreateControlContainer(sMainInfo);
+	oMainInfoControl.Bounds.SetParams(5, 5, 155, 0, true, true, true, false, -1, 200);
+	oMainInfoControl.Anchor = (g_anchor_top | g_anchor_right | g_anchor_left);
+	oMainControl.AddControl(oMainInfoControl);
+
+	this.m_oAvatarDiv = this.protected_CreateDivElement(oMainDiv, sAvatar);
+	var oAvatarControl = CreateControlContainer(sAvatar);
+	oAvatarControl.Bounds.SetParams(0, 5, 5, 0, false, true, true, false, 150, 200);
+	oAvatarControl.Anchor = (g_anchor_top | g_anchor_right);
+	oMainControl.AddControl(oAvatarControl);
+
+	this.m_oExtensionDiv = this.protected_CreateDivElement(oMainDiv, sExtension);
+	var oExtensionControl = CreateControlContainer(sExtension);
+	oExtensionControl.Bounds.SetParams(5, 205, 5, 5, true, true, true, true, -1, -1);
+	oExtensionControl.Anchor = (g_anchor_top | g_anchor_right | g_anchor_left | g_anchor_bottom);
+	oMainControl.AddControl(oExtensionControl);
+
+
+	// this.m_oContainerDiv = this.protected_CreateDivElement(oMainDiv, sDivId + "C");
+	// var oContainerControl = CreateControlContainer(sDivId + "C");
+	// oContainerControl.Bounds.SetParams(5, 5, 5, 5, true, true, true, true, -1, -1);
+	// oContainerControl.Anchor = (g_anchor_top |g_anchor_bottom | g_anchor_right | g_anchor_left);
+	// oMainControl.AddControl(oContainerControl);
 
 
 	oMainDiv.style.backgroundColor = "rgb(243, 243, 243)";
@@ -63,63 +85,66 @@ CKGSUserInfoWindow.prototype.OnUserDetails = function(oDetails)
 		if (-1 !== oDetails.user.flags.indexOf("a"))
 			this.OnUserAvatar();
 
-		this.private_AddConsoleMessage("UserName", oDetails.user.name);
-		this.private_AddConsoleMessage("Rank", oDetails.user.rank ? oDetails.user.rank : "-");
-		this.private_AddConsoleMessage("Last on", oDetails.lastOn);
-		this.private_AddConsoleMessage("Locale", oDetails.locale);
-		this.private_AddConsoleMessage("Name", oDetails.personalName);
+		this.private_AddMainInfo(oDetails);
 		this.private_AddInfo(oDetails.personalInfo);
 	}
 };
 CKGSUserInfoWindow.prototype.OnUserAvatar = function(oMessage)
 {
-	var oDiv = this.m_oContainerDiv;
 	var oImg = document.createElement("img");
-
-	oImg.align        = "right";
+	oImg.style.float  = "right";
 	oImg.style.width  = "141px";
 	oImg.style.height = "200px";
 	oImg.src          = "http://goserver.gokgs.com/avatars/" + this.m_sUserName + ".jpg";
-
-	oDiv.appendChild(oImg);
-
+	this.m_oAvatarDiv.appendChild(oImg);
 };
 CKGSUserInfoWindow.prototype.OnUserGameArchive = function(oMessage)
 {
+	console.log(oMessage);
+};
+CKGSUserInfoWindow.prototype.private_AddMainInfo = function(oDetails)
+{
+	var oDiv = this.m_oMainInfoDiv;
 
+	this.m_oMainInfoTable = document.createElement("table");
+
+	this.private_AddConsoleMessage("UserName", oDetails.user.name);
+	this.private_AddConsoleMessage("Rank", oDetails.user.rank ? oDetails.user.rank : "-");
+	this.private_AddConsoleMessage("Last on", oDetails.lastOn);
+	this.private_AddConsoleMessage("Locale", oDetails.locale);
+	this.private_AddConsoleMessage("Name", oDetails.personalName);
+
+	oDiv.appendChild(this.m_oMainInfoTable);
 };
 CKGSUserInfoWindow.prototype.private_AddConsoleMessage = function(sField, sText)
 {
-	var oDiv     = this.m_oContainerDiv;
-	var oTextDiv = document.createElement("div");
+	var oTable = this.m_oMainInfoTable;
 
-	var oTextSpan;
+	var oRow = document.createElement("tr");
+	oTable.appendChild(oRow);
 
-	if (sField)
-	{
-		oTextSpan                  = document.createElement("span");
-		oTextSpan.style.fontWeight = "bold";
-		oTextSpan.style.fontStyle  = "italic";
-		oTextSpan.textContent      = sField + ": ";
-		oTextDiv.appendChild(oTextSpan);
-	}
+	var oCell = document.createElement("td");
+	oRow.appendChild(oCell);
 
-	var aLines = SplitTextToLines(sText);
-	for (var nIndex = 0, nCount = aLines.length; nIndex < nCount; ++nIndex)
-	{
-		oTextSpan            = document.createElement("span");
-		oTextSpan.innerHTML  = aLines[nIndex];
+	var oTextSpan              = document.createElement("span");
+	oTextSpan.style.fontWeight = "bold";
+	oTextSpan.style.fontStyle  = "italic";
+	oTextSpan.textContent      = sField + ": ";
+	oCell.appendChild(oTextSpan);
 
-		oTextDiv.appendChild(oTextSpan);
-		oTextDiv.appendChild(document.createElement("br"));
-	}
+	oCell = document.createElement("td");
+	oRow.appendChild(oCell);
 
-	oDiv.appendChild(oTextDiv);
-	return oTextDiv;
+
+	oTextSpan             = document.createElement("span");
+	oTextSpan.textContent = sText;
+	oCell.appendChild(oTextSpan);
 };
 CKGSUserInfoWindow.prototype.private_AddInfo = function(sText)
 {
-	var oDiv     = this.m_oContainerDiv;
+	var oDiv     = this.m_oExtensionDiv;
+	oDiv.style.overflowY = "scroll";
+
 	var oTextDiv = document.createElement("div");
 
 	var oTextSpan;
@@ -131,10 +156,6 @@ CKGSUserInfoWindow.prototype.private_AddInfo = function(sText)
 	oTextDiv.appendChild(oTextSpan);
 
 	var oInfoDiv = document.createElement("div");
-	oInfoDiv.style.width  = "100%";
-	oInfoDiv.style.bottom = "0px";
-	oInfoDiv.style.overflowY = "scroll";
-
 	var aLines = SplitTextToLines(sText);
 	for (var nIndex = 0, nCount = aLines.length; nIndex < nCount; ++nIndex)
 	{
